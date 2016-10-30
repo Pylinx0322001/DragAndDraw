5de2f613acaf6bfa8721c33bbbef871bdd30fb28
Lee <171631592@qq.com>
2016-10-30 20:00:24 +0800

    onDraw

---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/BoxDrawingView.java
@@ -0,0 +1,62 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by DIY on 2016/10/30.
+ */
+public class BoxDrawingView extends View{
+    private static final String TAG="BoxDrawingView";
+
+    private Box mCurrentBox;
+    private List<Box> mBoxen=new ArrayList<>();
+
+    public BoxDrawingView(Context context){
+        this(context,null);
+    }
+
+    public BoxDrawingView(Context context, AttributeSet attrs){
+        super(context,attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current=new PointF(event.getX(),event.getY());
+        String action="";
+
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                action="ACTION_DOWN";
+                mCurrentBox=new Box(current);
+                mBoxen.add(mCurrentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                action="ACTION_MOVE";
+                if(mCurrentBox !=null){
+                    mCurrentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                action="ACTION_UP";
+                mCurrentBox=null;
+                break;
+            case MotionEvent.ACTION_CANCEL:
+                action="ACTION_CANCEL";
+                mCurrentBox=null;
+                break;
+        }
+
+        Log.i(TAG,action+"at x="+current.x+",y="+current.y);
+
+        return true;
+    }
+}
---
+++ app/src/main/java/com/bignerdranch/android/draganddraw/SingleFragmentActivity.java
@@ -0,0 +1,28 @@
+package com.bignerdranch.android.draganddraw;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by DIY on 2016/10/30.
+ */
+public abstract class SingleFragmentActivity extends AppCompatActivity{
+    protected abstract Fragment createFragment();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment);
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            manager.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+    }
+}
